f803524e1a9c29694c518c41c1b57fe964fa21da
Vladislovep <patr982@gmail.com>
2023-01-30 03:23:33 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,16 +1,8 @@
-import yolov5
 from yolov5.helpers import YOLOv5
-
-# load model
 
 
 class Yolo_v5:
-    def __init__(self, conf=0.25, iou=0.45, agnostic=False, multi_label=False, max_det=1000):
-        self.conf = conf
-        self.iou = iou
-        self.agnostic = agnostic
-        self.multi_label = multi_label
-        self.max_det = max_det
+    def __init__(self):
         self.model = YOLOv5('keremberke/yolov5s-csgo')
         self.model.load_model()
 
